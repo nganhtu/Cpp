b837c92a2c535ff7f8dc8d5b6178a9838fa13c72
nganhtu <nganhtu0712@gmail.com>
2021-04-20 18:40:00 +0700

    nháp: ngắt dòng với \r\n

--- nháp.cpp
+++ nháp.cpp
@@ -2,19 +2,22 @@
 
 using namespace std;
 
-struct Session
-{
-    int port;
-};
-
 int main()
 {
-    Session a = {5};
-    cout << a.port;
-
-    void *b = (void *)&a;
-
-    cout << ((Session *)b)->port;
+    char s[1000];
+    fgets(s, 1000, stdin);
+    s[strlen(s)] = '\0';
+    strcat_s(s, 1000, "\r\n\0");
+    printf("\"%s\"", s);
+    printf("%d\n", strlen(s));
+    if (strstr(s, "\r\n") != NULL)
+    {
+        printf("s has \\r\\n.\n");
+    }
+    else
+    {
+        printf("s has not \\r\\n.\n");
+    }
 
     return 0;
 }
