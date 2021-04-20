38dce7b60b5df85b751775d9a06b636232fa5091
nganhtu <nganhtu0712@gmail.com>
2021-04-20 18:52:35 +0700

    nháp: ngắt dòng với \r\n

--- nháp.cpp
+++ nháp.cpp
@@ -5,13 +5,12 @@ using namespace std;
 int main()
 {
     char s[1000];
-    fgets(s, 1000, stdin);
-
-    printf("%d\n", strlen(s));
+    fgets(s, 1000, stdin);     // "0123456789"
+    printf("%d\n", strlen(s)); // 11
     s[strlen(s) - 1] = '\0';
-    printf("%d\n", strlen(s));
+    printf("%d\n", strlen(s)); // 10
     strcat_s(s, 1000, "\r\n\0");
-    printf("%d\n", strlen(s));
+    printf("%d\n", strlen(s)); // 12
     printf("\"%s\"\n", s);
     if (strstr(s, "\r\n") != NULL)
     {
